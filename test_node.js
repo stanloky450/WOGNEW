866109959acb16413abc57a33614df90287a5860
stanloky450 <backlinkreator@gmail.com>
2026-01-17 14:27:18 +0100

    second  commit

--- test_node.js
+++ test_node.js
@@ -1,8 +1,8 @@
-console.log("NODE IS WORKING");
+console.log("NODE IS WORKING Fine");
 const fs = require("fs");
 try {
-  fs.writeFileSync("test_node.txt", "it works");
-  console.log("File written");
-} catch(e) {
-  console.error("File write error:", e);
+	fs.writeFileSync("test_node.txt", "it works");
+	console.log("File written");
+} catch (e) {
+	console.error("File write error:", e);
 }
